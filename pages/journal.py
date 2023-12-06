ee7715d440d68380b6610a79ac8f74dc341fb809
DerekDude77 <derek.charlton00@gmail.com>
2023-12-06 10:35:08 -0700

    Fixed journal bugs

--- pages/journal.py
+++ pages/journal.py
@@ -5,25 +5,31 @@ import os
 
 # Function to save entry to CSV
 def save_to_csv(title, date, text):
-    entry = {'Title': title, 'Date': date, 'Text': text}
+    # Format the date to Month-Day-Year
+    formatted_date = datetime.strftime(date, '%Y-%m-%d')
+
+    entry = pd.DataFrame([{'Title': title, 'Date': formatted_date, 'Text': text}])
     
-    # Check if the file exists
     if os.path.exists('journal_entries.csv'):
-        df = pd.read_csv('journal_entries.csv', parse_dates=['Date'], index_col=0)
+        df = pd.read_csv('journal_entries.csv')
     else:
         df = pd.DataFrame(columns=['Title', 'Date', 'Text'])
     
-    df = df.append(entry, ignore_index=True)
+    df = pd.concat([df, entry], ignore_index=True)
     df.to_csv('journal_entries.csv', index=False)
 
 # Function to filter entries based on title and date
 def filter_entries(df, title_filter, date_filter):
     filtered_df = df.copy()
-    
+
+    # Ensure 'Date' is in Month-Day-Year format
+    filtered_df['Date'] = pd.to_datetime(filtered_df['Date'], format='%Y-%m-%d', errors='coerce')
+
     if title_filter:
         filtered_df = filtered_df[filtered_df['Title'].str.contains(title_filter, case=False)]
     
     if date_filter:
+        # Convert the date_filter to the same format
         date_filter = datetime.strptime(date_filter, '%Y-%m-%d').date()
         filtered_df = filtered_df[filtered_df['Date'].dt.date == date_filter]
     
@@ -33,10 +39,16 @@ def filter_entries(df, title_filter, date_filter):
 def main():
     st.title("Journal App")
 
+    # Initialize session state for date filter if not present
+    if 'date_filter' not in st.session_state:
+        st.session_state['date_filter'] = None
+
     # Sidebar
     st.sidebar.header("Filters")
     title_filter = st.sidebar.text_input("Filter by Title:")
-    date_filter = st.sidebar.date_input("Filter by Date:")
+
+    # Date input with a key to link it to the session state
+    date_filter = st.sidebar.date_input("Filter by Date:", key='date_filter')
 
     # Form for adding entries
     st.header("Add Entry")
@@ -54,7 +66,8 @@ def main():
         # Read CSV and filter entries
         if os.path.exists('journal_entries.csv'):
             df = pd.read_csv('journal_entries.csv', parse_dates=['Date'])
-            filtered_df = filter_entries(df, title_filter, str(date_filter))
+            # Use the session state variable for filtering
+            filtered_df = filter_entries(df, title_filter, st.session_state['date_filter'])
 
             # Display filtered entries
             for index, row in filtered_df.iterrows():
@@ -67,5 +80,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
